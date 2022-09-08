54fd51e475711633b077b805e1102c9bc13ea893
gabremlee <gabriel.remilekun@gmail.com>
2022-09-08 05:44:32 -0700

    Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- listing all shows in hbtn_0d_tvshows with
+-- with at least one genre liked
+-- tv_shows.title and tv_show_genres.genre_id
+-- ordered ascendingly by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
