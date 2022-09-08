7dc3ea4cb7181975860c37d2d7cefd35763f6997
gabremlee <gabriel.remilekun@gmail.com>
2022-09-08 05:29:14 -0700

    Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creating a database hbtn_0d_usa with table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id));
