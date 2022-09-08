7fb8da2e55c113f56f208518ed73391017ab9ff0
gabremlee <gabriel.remilekun@gmail.com>
2022-09-08 06:35:35 -0700

    Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- listing all shows from hbtn_0d_tvshows_rate by their rating
+-- to display tv_shows.title - rating sum
+-- ordered in descending by rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings
+     ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
