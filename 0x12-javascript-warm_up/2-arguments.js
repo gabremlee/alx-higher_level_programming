2c88bf018651f09f7da762b5dd8bc11ca6c10d6a
root <gabriel.remilekun@gmail.com>
2022-10-04 07:36:25 -0700

    updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-
-const  { argv } = require('node:process');
-let count = 0;
-argv.forEach((val) => {
-	count++;
-});
-if (count > 3) {
-	console.log("Arguments found");
-} else if (count == 3) {
-	console.log("Argument found")
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log("No argument");
+  console.log('Arguments found');
 }
