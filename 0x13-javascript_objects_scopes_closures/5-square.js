9dcb5498feafc1f331eea6c1a5e775665a34aba3
root <gabriel.remilekun@gmail.com>
2022-10-06 04:42:48 -0700

    Square #0

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (width) {
+    super(width);
+    this.width = width;
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  double () {
+    this.width *= 2;
+  }
+}
+
+module.exports = Square;
